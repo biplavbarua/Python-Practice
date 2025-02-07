89f077f4e22d7613ea9d9c052ed4cbea881b70da
biplavbarua <innocentsavage.bb@gmail.com>
2025-02-07 10:52:44 +0530

    Merged DataFrames

--- DataFrames_Merge.py
+++ DataFrames_Merge.py
@@ -11,7 +11,21 @@ df2 = pd.DataFrame({
     'Salary': [50000, 60000, 55000]
     })
 
+df3 = pd.DataFrame({
+    'EmployeeID': [101, 102, 105],
+    'Age': [25, 30, 35]
+    })
+
 #Merge both DataFrames on EmployeeID
 
 merged_df = pd.merge(df1, df2, on="EmployeeID",how="left")
 print(merged_df)
+
+#Merge all DataFrames
+
+new_merged_df = pd.merge(merged_df, df3, on="EmployeeID", how="left")
+print(new_merged_df)
+
+#Cross-Product
+mergednew_df = merged_df.merge(df3, how="cross")
+print(mergednew_df)
